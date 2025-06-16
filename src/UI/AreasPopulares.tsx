d72baea28d492320c28635baebf2875d62705cef
luifer991 <luifer991@gmail.com>
2025-06-16 07:32:02 -0500

    feat: enhance AreasPopulares component with dynamic area cards and improved layout

--- src/UI/AreasPopulares.tsx
+++ src/UI/AreasPopulares.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import {
 	Card,
 	CardAction,
@@ -8,23 +9,68 @@ import {
 	CardTitle,
 } from '@/components/ui/card';
 
+const areas = [
+	{
+		id: 1,
+		nombre: 'Los Laureles',
+		descripcion: 'Cancha de football',
+		imageUrl: '/cancha-futbol.jpg',
+		precio: 100,
+	},
+	{
+		id: 2,
+		nombre: 'La ortencia',
+		descripcion: 'cancha de tenis',
+		imageUrl: '/tenis.jpg',
+		precio: 200,
+	},
+	{
+		id: 3,
+		nombre: 'Gym',
+		descripcion: 'Gymnasio',
+		imageUrl: '/gym.jpg',
+		precio: 300,
+	},
+];
+
 export default function AreasPopulares() {
 	return (
 		<>
-			<h4>Áreas deportivas mas populares</h4>
-			<Card>
-				<CardHeader>
-					<CardTitle>Card Title</CardTitle>
-					<CardDescription>Card Description</CardDescription>
-					<CardAction>Card Action</CardAction>
-				</CardHeader>
-				<CardContent>
-					<p>Card Content</p>
-				</CardContent>
-				<CardFooter>
-					<p>Card Footer</p>
-				</CardFooter>
-			</Card>
+			<h4 className="text-5xl font-bold mb-12">
+				Nuestras Áreas deportivas
+			</h4>
+			<div className="grid grid-cols-3 gap-8">
+				{areas.map((area) => (
+					<Card className="w-70 h-auto" key={area.id}>
+						<CardHeader>
+							<CardTitle className="flex justify-start">
+								{area.nombre}
+							</CardTitle>
+							<CardDescription className="flex justify-start">
+								{area.descripcion}
+							</CardDescription>
+							<CardAction>
+								<Button
+									className="hover:bg-violet-500"
+									onClick={() => console.log('reservar')}
+								>
+									Reservar
+								</Button>
+							</CardAction>
+						</CardHeader>
+						<CardContent>
+							<img
+								className="aspect-square w-80 h-auto"
+								src={area.imageUrl}
+								alt={area.nombre}
+							/>
+						</CardContent>
+						<CardFooter>
+							<p> ${area.precio}/h </p>
+						</CardFooter>
+					</Card>
+				))}
+			</div>
 		</>
 	);
 }
