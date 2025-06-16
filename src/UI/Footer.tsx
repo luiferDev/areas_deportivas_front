f2e6fd43a53d6485d17389ca199f01497d17d6e7
luifer991 <luifer991@gmail.com>
2025-06-16 07:32:13 -0500

    feat: add Footer component for displaying copyright information

---
+++ src/UI/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+	return <>
+		<footer>
+				<div className="flex justify-center items-center mt-16">
+					<p className="text-gray-500 text-sm">
+					&copy;
+						{new Date().getFullYear()} SGR. Todos los derechos reservados.
+					</p>
+				</div>
+		</footer>
+	</>;
+}
