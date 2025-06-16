71fb4caeb7523edca0740b884fe6090a1ede4029
luifer991 <luifer991@gmail.com>
2025-06-16 08:47:24 -0500

    feat: add SignUp route to main application for user registration

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
 import App from './App.tsx';
 import { Login } from './Links/Login.tsx';
+import { SignUp } from './Links/SingUp.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
 			<Routes>
 				<Route path="/" element={<App />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/sign-up" element={<SignUp />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>
