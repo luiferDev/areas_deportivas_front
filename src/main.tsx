090353fa2ddb23a088c4a225398ebec88f54e52e
luifer991 <luifer991@gmail.com>
2025-06-16 07:31:16 -0500

    feat: add Login route to main application

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
 import App from './App.tsx';
+import { Login } from './Links/Login.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>
