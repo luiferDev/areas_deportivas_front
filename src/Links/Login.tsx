75e7be3a43152bd1dde4be9f4ab078418acf8e93
luifer991 <luifer991@gmail.com>
2025-06-16 10:08:29 -0500

    feat: update Login component with localized text and improved layout

--- src/Links/Login.tsx
+++ src/Links/Login.tsx
@@ -17,57 +17,74 @@ import { Link } from 'react-router';
 export function Login() {
 	return (
 		<>
-			<NavigationMenuComponent />
-			<Card className="w-full max-w-sm mt-16">
-				<CardHeader>
-					<CardTitle className="flex justify-start">
-						Login to your account
-					</CardTitle>
-					<CardDescription className="flex justify-start text-start">
-						Enter your email below to login to your account
-					</CardDescription>
-					<CardAction>
-						<Button variant="outline">
-							<Link to={'/sign-up'}>Sign Up</Link>
-						</Button>
-					</CardAction>
-				</CardHeader>
-				<CardContent>
-					<form>
-						<div className="flex flex-col gap-6">
-							<div className="grid gap-2">
-								<Label htmlFor="email">Email</Label>
-								<Input
-									id="email"
-									type="email"
-									placeholder="m@example.com"
-									required
-								/>
-							</div>
-							<div className="grid gap-2">
-								<div className="flex items-center">
-									<Label htmlFor="password">Password</Label>
-									<a
-										href="#"
-										className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-									>
-										Forgot your password?
-									</a>
+			<main className="flex flex-row justify-between">
+				<NavigationMenuComponent />
+				<Card className="w-full max-w-sm mt-16">
+					<CardHeader>
+						<CardTitle className="flex justify-start">
+							Ingresa a tu cuenta
+						</CardTitle>
+						<CardDescription className="flex justify-start text-start">
+							Ingresa tu Email para entrar a tu cuenta
+						</CardDescription>
+						<CardAction>
+							<Button variant="outline">
+								<Link to={'/sign-up'}>Sign Up</Link>
+							</Button>
+						</CardAction>
+					</CardHeader>
+					<CardContent>
+						<form>
+							<div className="flex flex-col gap-6">
+								<div className="grid gap-2">
+									<Label htmlFor="email">Email</Label>
+									<Input
+										id="email"
+										type="email"
+										placeholder="m@example.com"
+										required
+									/>
 								</div>
-								<Input id="password" type="password" required />
+								<div className="grid gap-2">
+									<div className="flex items-center">
+										<Label htmlFor="password">
+											Password
+										</Label>
+										<a
+											href="#"
+											className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+										>
+											Olvidase tu password?
+										</a>
+									</div>
+									<Input
+										id="password"
+										type="password"
+										required
+									/>
+								</div>
 							</div>
-						</div>
-					</form>
-				</CardContent>
-				<CardFooter className="flex-col gap-2">
-					<Button type="submit" className="w-full">
-						Login
-					</Button>
-					<Button variant="outline" className="w-full">
-						Login with Google
-					</Button>
-				</CardFooter>
-			</Card>
+						</form>
+					</CardContent>
+					<CardFooter className="flex-col gap-2">
+						<Button type="submit" className="w-full">
+							Entrar
+						</Button>
+						<Button variant="outline" className="w-full">
+							Login with Google
+						</Button>
+					</CardFooter>
+				</Card>
+				<aside className="bg-violet-800 h-[400px] w-1/2 text-start justify-start align-start mt-16">
+					<div className="ml-20 pt-20">
+						<h3 className="text-5xl font-bold">Bienvenido/a</h3>
+						<p className="text-3xl w-80 mt-4">
+							Ingresa para reservar tus áreas deportivas
+							facilmente
+						</p>
+					</div>
+				</aside>
+			</main>
 			<Footer />
 		</>
 	);
