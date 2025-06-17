e6038d6dfacccda0c80e24eba5917b4a7ba25c81
luifer991 <luifer991@gmail.com>
2025-06-17 16:57:45 -0500

    refactor: update auth interceptor to use Zustand for token management

--- src/fetch/auh.ts
+++ src/fetch/auh.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useAuthStore } from '@/store/Auth';
 
 const baseUrl = 'http://localhost:5223';
 
@@ -9,10 +10,15 @@ export const auth = axios.create({
 	},
 });
 
+
 auth.interceptors.request.use((config) => {
-	const token = localStorage.getItem('token');
+	const { token } = useAuthStore.getState(); // 👈 accede a Zustand directamente
+
 	if (token) {
+		config.headers = config.headers || {}; // asegúrate de que exista
 		config.headers.Authorization = `Bearer ${token}`;
 	}
+
+	console.log('Request config:', config); // vuelve a verificar después del cambio
 	return config;
 });
