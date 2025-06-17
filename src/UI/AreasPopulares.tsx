c0a3598eda6cc8e8c4ac13dfbdcbdd6b0616acda
luifer991 <luifer991@gmail.com>
2025-06-16 22:36:53 -0500

    feat: fetch sports areas from API and update component state

--- src/UI/AreasPopulares.tsx
+++ src/UI/AreasPopulares.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { getAreas, type Areas } from '@/fetch/fetchAreas';
 import {
 	Card,
 	CardAction,
@@ -8,68 +9,75 @@ import {
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card';
-
-const areas = [
-	{
-		id: 1,
-		nombre: 'Los Laureles',
-		descripcion: 'Cancha de football',
-		imageUrl: '/cancha-futbol.jpg',
-		precio: 100,
-	},
-	{
-		id: 2,
-		nombre: 'La ortencia',
-		descripcion: 'cancha de tenis',
-		imageUrl: '/tenis.jpg',
-		precio: 200,
-	},
-	{
-		id: 3,
-		nombre: 'Gym',
-		descripcion: 'Gymnasio',
-		imageUrl: '/gym.jpg',
-		precio: 300,
-	},
-];
+import { useEffect, useState } from 'react';
 
 export default function AreasPopulares() {
+	const [areas, setAreas] = useState<Areas[]>([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		const fetchAreas = async () => {
+			try {
+				const data = await getAreas();
+				setAreas(data);
+			} catch (err) {
+				setError((err as Error).message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchAreas();
+	}, []);
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+	if (error) {
+		return <div>Error: {error}</div>;
+	}
+
 	return (
 		<>
 			<h4 className="text-5xl font-bold mb-12">
 				Nuestras Áreas deportivas
 			</h4>
 			<div className="grid grid-cols-3 gap-8">
-				{areas.map((area) => (
-					<Card className="w-70 h-auto" key={area.id}>
-						<CardHeader>
-							<CardTitle className="flex justify-start">
-								{area.nombre}
-							</CardTitle>
-							<CardDescription className="flex justify-start">
-								{area.descripcion}
-							</CardDescription>
-							<CardAction>
-								<Button
-									className="hover:bg-violet-500"
-									onClick={() => console.log('reservar')}
-								>
-									Reservar
-								</Button>
-							</CardAction>
-						</CardHeader>
-						<CardContent>
-							<img
-								className="aspect-square w-80 h-auto"
-								src={area.imageUrl}
-								alt={area.nombre}
-							/>
-						</CardContent>
-						<CardFooter>
-							<p> ${area.precio}/h </p>
-						</CardFooter>
-					</Card>
-				))}
+				{Array.isArray(areas) ? (
+					areas.map((area) => (
+						<Card className="w-70 h-auto" key={area.id}>
+							<CardHeader>
+								<CardTitle className="flex justify-start">
+									{area.nombre}
+								</CardTitle>
+								<CardDescription className="flex justify-start">
+									{area.description}
+								</CardDescription>
+								<CardAction>
+									<Button
+										className="hover:bg-violet-500"
+										onClick={() => console.log('reservar')}
+									>
+										Reservar
+									</Button>
+								</CardAction>
+							</CardHeader>
+							<CardContent>
+								<img
+									className="aspect-square w-80 h-auto"
+									src={area.imageUrl}
+									alt={area.nombre}
+								/>
+							</CardContent>
+							<CardFooter>
+								<p> ${area.precio}/h </p>
+							</CardFooter>
+						</Card>
+					))
+				) : (
+					<p>No se encontraron áreas disponibles.</p>
+				)}
 			</div>
 		</>
 	);
