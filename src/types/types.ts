32a9f9bcbc068a2b7545b12bc9fc5831b7297bf3
luifer991 <luifer991@gmail.com>
2025-06-19 09:55:30 -0500

    feat: add types for Reservacion, Reserva and AreaDeportiva

---
+++ src/types/types.ts
@@ -0,0 +1,30 @@
+export interface Reservacion {
+	reserva: Reserva;
+	areaDeportiva: AreaDeportiva;
+}
+
+export interface Reserva {
+	id: string;
+	fecha: string;
+	horaInicio: string;
+	horaFin: string;
+	estado: string;
+}
+
+export interface AreaDeportiva {
+	id: number;
+	nombre: string;
+	description: string;
+	tipoArea: string;
+	disponibilidad: boolean;
+	imageUrl: string;
+	precio: number;
+}
+
+export interface Areas {
+	id: number;
+	nombre: string;
+	description: string;
+	imageUrl: string;
+	precio: number;
+}
