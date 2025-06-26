51c60b317e38aa8b7d0e70ef915a9d8494ac0047
luifer991 <luifer991@gmail.com>
2025-06-26 11:41:57 -0500

    chore: add setupTests file and configure Vitest for testing environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import path from 'path';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/setupTests.ts',
+		css: true,
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
